966ee2343335784c62bd199bdadd7f3adc683198
Adrian Chrysanthou <adrianvfx@gmail.com>
2022-01-30 00:21:52 -0500

    remake

--- com.f00d4tehg0dz.cookieclicker.sdPlugin/js/app.js
+++ com.f00d4tehg0dz.cookieclicker.sdPlugin/js/app.js
@@ -1,98 +1,168 @@
-let intervals = {};
-    if ($SD) {
-        const actionName = "com.f00d4tehg0dz.cookieclicker.action";
+if ($SD) {
+	const actionName = "com.f00d4tehg0dz.cookieclicker.action";
+	var num = 0;
+	$SD.on("connected", function(jsonObj) {
+		console.log("Connected!");
 
-        $SD.on("connected", function (jsonObj) {
-            console.log("Connected!");
-      
-        });
-        
-        $SD.on(actionName + ".willAppear", function (jsonObj) {
-            let settings = jsonObj.payload.settings;
-            if(settings.updateClick){
-                initiateStatus(jsonObj.context, jsonObj.payload.settings);
-            }  
-        });
+	});
 
-        $SD.on(actionName + ".sendToPlugin", function (jsonObj) {
-            $SD.api.setSettings(jsonObj.context, jsonObj.payload);
-            initiateStatus(jsonObj.context, jsonObj.payload);
-        });
+	$SD.on(actionName + ".willAppear", function(jsonObj) {
+		let settings = jsonObj.payload.settings;
+		if (settings.automaticRefresh) {
+			initiateStatus(jsonObj.context, jsonObj.payload.settings);
+		}
+	});
 
-        // When pressed, CookeClicker status gets updated!
-        $SD.on(actionName + ".keyUp", function (jsonObj) {
-          
-            initiateStatus(jsonObj.context, jsonObj.payload.settings);
-            console.log();
-        });
-    
-        function initiateStatus(context, settings) {
-            if (intervals[context]) {
-                let interval = intervals[context];
-                clearInterval(interval);
-            }
+	$SD.on(actionName + ".sendToPlugin", function(jsonObj) {
+		$SD.api.setSettings(jsonObj.context, jsonObj.payload);
+		initiateStatus(jsonObj.context, jsonObj.payload);
+	});
 
-            // Initial call for the first time
-            setTitleStatus(context, settings);
+	// When pressed, Cookie Clicker Activates!
+	$SD.on(actionName + ".keyUp", function(jsonObj) {
+		initiateStatus(jsonObj.context, jsonObj.payload.settings);
+		console.log();
+	});
 
-            // Start Canvas
-            canvas = document.createElement('canvas');  
+	function initiateStatus(context, settings) {
+
+		// Initial call for the first time
+		setTitleStatus(context, settings);
+
+		// Start Canvas
+		canvas = document.createElement('canvas');
+		canvas.width = 144;
+		canvas.height = 144;
+		block = new Block(canvas);
+		ctx = canvas.getContext('2d');
+        img = document.getElementById("bg");
+        ctx.drawImage(img, 0, 0);
+		// Set the text font styles
+		ctx.font = 'bold 20px Arial';
+		ctx.fillStyle = "white";
+		ctx.textAlign = 'left';
+		
+	}
+
+	function setTitleStatus(context, settings) {
+		$SD.api.setTitle(context, "Updating");
+	
+        getResults(result => resultCallback(result, context, settings));
+	}
+
+   
+    function numbersBoard(result) {
+        var resultString = result.number,
+        textWidth = ctx.measureText(resultString).width;
+        ctx.font = 'bold 48px Arial';
+		ctx.textAlign = 'left';
+        ctx.fillStyle = "#deff00";
+		return resultString
+        // ctx.fill();
+        // ctx.fillText(resultString , (canvas.width/2) - (textWidth / 2), 80);
+    }
+
+    function titleBoard(result) {
+        var resultString = result.title,
+        textWidth = ctx.measureText(resultString).width;
+        ctx.font = 'bold 30px Arial';
+		ctx.textAlign = 'left';
+        ctx.fillStyle = "#deff00";
+		return resultString
+        // ctx.fill();
+        // ctx.fillText(resultString , (canvas.width/2) - (textWidth / 2), 120);
+    }
+
+	function resultCallback(result, context) {
+		
+		if (!result.hasOwnProperty("Object")) {
+
+            // load bg-image
+            canvas = document.createElement('canvas');
             canvas.width = 144;
             canvas.height = 144;
             block = new Block(canvas);
-            ctx = canvas.getContext('2d');
+            ctx = canvas.getContext("2d");
+            img = document.getElementById("bg");
+            ctx.drawImage(img, 0, 0);
 
-            // Set the text font styles
-            ctx.font = 'bold 18px Arial';
-            ctx.fillStyle = "white";
-            ctx.textAlign = 'left';
-            ctx.textBaseline = 'middle';
-
-           
+			textWidth = ctx.measureText(numbersBoard(result)).width;
+			ctx.fillText(numbersBoard(result, context), (canvas.width/2) - (textWidth / 2), 80);
             
-        }
+			textWidth = ctx.measureText(titleBoard(result)).width;
+			ctx.fillText(titleBoard(result, context), (canvas.width/2) - (textWidth / 2), 120);
+            
+            ctx.font = 'bold 30px Arial';
+		    ctx.fillStyle = "#deff00";
+		    ctx.textAlign = 'left';
+		    ctx.textBaseline = 'middle';
 
-        function setTitleStatus(context, settings) {
-             //initial number of cookies    
-             var num = 0;
+            ctx.fill();
+            textWidth = ctx.measureText("🍪").width;
+            ctx.fillText("🍪", (canvas.width/2) - (textWidth / 2), 20);
+	
+            $SD.api.setTitle(context, '', null);
+            $SD.api.setImage(
+                context,
+                block.getImageData()
+            );
+			
+			return;
+		}
+	}
 
-             var cookie = document.getElementById("cookie");
- 
-             function cookieClick() {
- 
-                 num += 1;
- 
-                 var numbers = document.getElementById("cookieNumbers");
-                 
-                 //when you level up your title changes
-                 var cookieTitle = document.getElementById("cookieTitle");
-                 
-                 numbers.innerHTML = num;      
-                 // 2x
-                 if(num >= 30 ){
-                     num += 2;
-                     cookieTitle.innerHTML = "Granny Level";
-                 }
- 
-                 // 10x
-                 if(num >= 500) {
-                     num += 10;
-                     cookieTitle.innerHTML = "Factory Level";
-                 }
- 
-                 // 30x
-                 if(num >= 1000) {
-                     num += 30;
-                     cookieTitle.innerHTML = "Plant Level";
-                 }
- 
-                 // 1000x
-                 if(num >= 100000) {
-                     num += 1000;
-                     cookieTitle.innerHTML = "Super-Plant Level";
-                 }
-             }
-            $SD.api.setTitle(context, "Updating");
-                
-        } 
+    function destroyCanvas() {
+        canvas = document.getElementById('canvas');
+        ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
+
+	function getResults(updateTitleFn) {
+        
+        // 1x
+        if (num >= 0) {
+			num += 1;
+            updateTitleFn(JSON.parse(JSON.stringify({
+                "number": num,
+                "title": "Grandma",
+            })));  
+		}
+
+		// 2x
+		if (num >= 30) {
+			num += 2;
+			updateTitleFn(JSON.parse(JSON.stringify({
+                "number": num,
+                "title": "Baker",
+            })));  
+		}
+
+		// 10x
+		if (num >= 500) {
+			num += 10;
+			updateTitleFn(JSON.parse(JSON.stringify({
+                "number": num,
+                "title": "Factory",
+            })));  
+		}
+
+		// 30x
+		if (num >= 1000) {
+			num += 30;
+			updateTitleFn(JSON.parse(JSON.stringify({
+                "number": num,
+                "title": "Plant",
+            })));  
+		}
+
+		// 1000x
+		if (num >= 100000) {
+			num += 1000;
+            updateTitleFn(JSON.parse(JSON.stringify({
+                "number": num,
+                "title": "Super-Plant",
+            })));      
+		}       
+	
+	}
+}
